ca3123af0ffc8bfcbb2fedea0a6bfe588216cf43
AlexisMerienne <alexis.merienne@gmail.com>
2022-05-06 13:07:22 +0200

    changement header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-      <h5><strong>Impactomètre carbone du <span style="color:#6887ff">numérique</span></strong></h5>
+      <h5 style="max-width: 300px"><strong>Impactomètre carbone du <span style="color:#6887ff">numérique</span></strong></h5>
     <div id="logo-img" alt="logo-i3S" v-on:click="goToI3s">
       <img id="img-logo-i3s" src="../../assets/NewlogoI3S_0.png" alt="I3S logo" width="100%">
     </div>
